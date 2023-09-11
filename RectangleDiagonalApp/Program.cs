88037e18e62eda3f7bd18702b3515341ed01a525
Grzegorz Kozłowski <grzegorz.kozlowski2@gmail.com>
2023-09-11 15:40:42 +0200

    Add project files.

---
+++ RectangleDiagonalApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace RectangleDiagonalApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to RectangleDiagonalApp!");
+            Console.WriteLine("\r");
+            double rectangleWidth = 5;
+            double rectangleHeight = 7;
+            double rectangleDiagonal = Math.Sqrt(Math.Pow(rectangleWidth, 2) + Math.Pow(rectangleHeight, 2));
+            Console.WriteLine($"The length of the diagonal of a rectangle with dimensions {rectangleWidth} and {rectangleHeight} is: {rectangleDiagonal}");
+        }
+    }
+}
